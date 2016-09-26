8b7e8fdcddce624c07c9d28462f1853fe17dca8f
Damien Thenot <damien.thenot@gmail.com>
2016-09-26 17:49:02 +0200

    Ajout de quelques tests sur les tableaux dynamiques

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,20 @@
 using namespace std;
 #include <iostream>
 
+int* nombrePremier(int n){
+  int* T = new int[n];
+  for(int i = 0; i<n; i++){
+    T[i] = i;
+  }
+  return T;
+}
+
 int main(){
-  cout<<"Test"<<endl;
+  cout<<"Test sur les tableaux : "<<endl;
+  int n = 10;
+  int* T = nombrePremier(n);
+  for(int i = 0; i<n; i++){
+    cout<<T[i]<<endl;
+  }
+  delete T;
 }

ee912675ec0ce72a5ffc13d2734a095788a7b8c5
Damien Thenot <damien.thenot@gmail.com>
2016-09-27 18:54:25 +0200

    Mise en place de l'algorithme du crible d'Eratosthène et finition du programme. TODO: regles de compilation et explication du programme dans le README

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,23 @@
 using namespace std;
 #include <iostream>
+#include <stdlib.h>
 #include "NombrePremier.cpp"
 
-int main(){
-  int n = 0;
-  cout<<"Veuillez entrez une valeur jusqu'à laquel calculer les nombres premiers : ";
-  cin>>n;
-  NombrePremier* T = new NombrePremier(n);
-  T->nombrePremierInitialisation();
-  T->afficher();
-  delete T;
+int main(int argc, char *argv[]){
+
+  if(argc > 2){
+    cerr<<"Le programme ne prend qu'un seul argument entier."<<endl;
+    exit(0);
+  }
+  else if(argc < 2){
+    cerr<<"Le programme prend un entier en entrée qui représente le nombre limite maximal."<<endl;
+    cerr<<" -./a.out [entier]"<<endl;
+    exit(0);
+  }
+  else{
+    int n = atoi(argv[1]);
+
+    NombrePremier T(n);
+    T.afficherOP();
+  }
 }
--- NombrePremier.cpp
+++ NombrePremier.cpp
@@ -9,17 +9,43 @@ private:
 
 public:
 
+    //Le constructeur prend n (un entier) qui représente la limite maximale jusqu'à laquel le programme va calculer les
+    //nombres premiers.
+    //Il appelle ensuite nombrePremierInitialisation() et nombrePremierCalcul().
     NombrePremier(int n){
       this->T = new int[n];
       this->n = n;
+      this->nombrePremierInitialisation();
+      this->nombrePremierCalcul();
     }
 
+    //Initialise tout le tableau T à 1.
     void nombrePremierInitialisation(){
       for(int i = 0; i<this->n; i++){
         this->T[i] = 1;
       }
     }
 
+    //Implémentation de l'algorithme du crible d'Eratosthène qui calcul les nombres premiers.
+    //Si un nombre est premier, il laisse T[id] à 1. Sinon il le met à 0. Il met ensuite tous les multiples de ce nombre premier
+    //à 0. id représente le chiffre. On saute les id 0 et 1 sinon tout le reste du tableau serait mis à 0.
+    void nombrePremierCalcul(){
+        int i = 2,j;
+        //this->T[n] : tableaux d'entiers (ou de booléens) initialisé en 1.
+
+        while((i*i)<this->n){
+          if(this->T[i]){
+            j = 2*i;
+            while(j < this->n){
+              this->T[j] = 0;
+              j+=i;
+            }
+          }
+          i++;
+        }
+    }
+
+    //Fonction afficher qui affiche tout le tableau, nombre premier ou composé.
     void afficher(){
       for(int i = 0; i<this->n; i++){
         cout<<i<<" -> "<<this->T[i]<<" | ";
@@ -29,6 +55,16 @@ public:
       }
     }
 
+    //Affiche une liste des nombres premiers sous la forme [nombre premier]\n
+    void afficherOP(){
+      for(int i = 0; i<this->n; i++){
+        if(this->T[i]){
+          cout<<i<<endl;
+        }
+      }
+    }
+
+    //Destructeur, détruit juste le tableau dynamique après l'éxécution du programme.
     ~NombrePremier(){
       delete this->T;
     }
